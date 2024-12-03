4f3899f52edd68b640f933e049be834d1c8ee88c
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-12-03 14:59:50 -0500

    app/tests/test_endpoints.py: Change tests

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -3,7 +3,18 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_get_suggestions():
-    response = client.post("/suggestions", json={"user_id": 1})
+
+def test_get_suggestions_genre_success():
+    response = client.post("/suggestions/genre", json={"genres": ["Crime"]})
     assert response.status_code == 200
     assert "suggestions" in response.json()
+
+
+def test_get_suggestions_genre_422():
+    response = client.post("/suggestions/genre", json={"s12": [1234]})
+    assert response.status_code == 422
+
+
+# def test_get_suggestions_genre_500():
+#     response = client.post("/suggestions/genre", json={"s12": ["asdasfa"]})
+#     assert response.status_code == 500
