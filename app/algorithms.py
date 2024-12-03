55f4b45db8de2379bbe658f223f7adddc98b923e
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-12-03 14:58:00 -0500

    Delete unnecesary files

--- app/algorithms.py
+++
@@ -1,8 +0,0 @@
-def suggest_movies(preferences):
-    """Simple algorithm to suggest movies based on user preferences."""
-    genres = [pref.genre for pref in preferences]
-    # Example: Return top 3 movies for each preferred genre
-    suggestions = []
-    for genre in genres:
-        suggestions.extend(crud.get_movies_by_genre(db, genre)[:3])
-    return suggestions
