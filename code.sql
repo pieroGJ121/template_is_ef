17e45910d55a79acdc853cd8ccadfdc0f493bcfe
pieroGJ121 <piero.guerrero@utec.edu.pe>
2024-12-03 15:00:24 -0500

    code.sql: Add sql insert commands

---
+++ code.sql
@@ -0,0 +1,18 @@
+INSERT INTO movies (id, title, genre, director, writer, studio, running_time, language, release_date) VALUES
+(1, 'Inception', 'Sci-Fi', 'Christopher Nolan', 'Christopher Nolan', 'Warner Bros.', 148, 'English', '2010-07-16'),
+(2, 'Parasite', 'Thriller', 'Bong Joon-ho', 'Bong Joon-ho, Han Jin-won', 'CJ Entertainment', 132, 'Korean', '2019-05-30'),
+(3, 'The Lion King', 'Animation', 'Roger Allers, Rob Minkoff', 'Irene Mecchi, Jonathan Roberts, Linda Woolverton', 'Walt Disney Pictures', 88, 'English', '1994-06-24'),
+(4, 'Pulp Fiction', 'Crime', 'Quentin Tarantino', 'Quentin Tarantino, Roger Avary', 'Miramax Films', 154, 'English', '1994-10-14'),
+(5, 'Spirited Away', 'Fantasy', 'Hayao Miyazaki', 'Hayao Miyazaki', 'Studio Ghibli', 125, 'Japanese', '2001-07-20');
+
+INSERT INTO movies (id, title, genre, director, writer, studio, running_time, language, release_date) VALUES
+(6, 'The Godfather', 'Crime', 'Francis Ford Coppola', 'Mario Puzo, Francis Ford Coppola', 'Paramount Pictures', 175, 'English', '1972-03-24'),
+(7, 'The Dark Knight', 'Action', 'Christopher Nolan', 'Jonathan Nolan, Christopher Nolan', 'Warner Bros.', 152, 'English', '2008-07-18'),
+(8, 'Titanic', 'Romance', 'James Cameron', 'James Cameron', '20th Century Fox', 195, 'English', '1997-12-19'),
+(9, 'Avengers: Endgame', 'Action', 'Anthony Russo, Joe Russo', 'Christopher Markus, Stephen McFeely', 'Marvel Studios', 181, 'English', '2019-04-26'),
+(10, 'Schindler’s List', 'Historical', 'Steven Spielberg', 'Steven Zaillian', 'Universal Pictures', 195, 'English', '1993-12-15'),
+(11, 'Coco', 'Animation', 'Lee Unkrich, Adrian Molina', 'Adrian Molina, Matthew Aldrich', 'Pixar Animation Studios', 105, 'English', '2017-11-22'),
+(12, 'The Matrix', 'Sci-Fi', 'Lana Wachowski, Lilly Wachowski', 'Lana Wachowski, Lilly Wachowski', 'Warner Bros.', 136, 'English', '1999-03-31'),
+(13, 'Gladiator', 'Historical', 'Ridley Scott', 'David Franzoni, John Logan, William Nicholson', 'DreamWorks Pictures', 155, 'English', '2000-05-05'),
+(14, 'Amélie', 'Romantic Comedy', 'Jean-Pierre Jeunet', 'Jean-Pierre Jeunet, Guillaume Laurant', 'UGC-Fox Distribution', 122, 'French', '2001-04-25'),
+(15, 'The Grand Budapest Hotel', 'Comedy', 'Wes Anderson', 'Wes Anderson', 'Fox Searchlight Pictures', 99, 'English', '2014-03-28');
