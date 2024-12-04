ba7577dbee7e38b7651704e943d0cc7f409550ca
GonzaloDST <gonzalo.suarez@utec.edu.pe>
2024-12-03 21:03:56 -0500

    update2

--- app/main.py
+++ app/main.py
@@ -1,30 +1,65 @@
 from fastapi import FastAPI, HTTPException, Depends
-from app.database import engine, Base, get_db
 from sqlalchemy.orm import Session
-from app import crud
-from app.schemas import Movie, Suggestion, Genres
-from app.logger import logger
+from app.database import get_db
+from app.models import User, Ticket, Booking, Purchase, Cancelation
+from app.schemas import Booking_schema, User_and_ticket_schema
 
-Base.metadata.create_all(bind=engine)
+app = FastAPI()
 
-app = FastAPI(title="Movie Suggestion API", docs_url="/swagger")
+@app.post("/ticket/purchase")
+def make_purchase(booking_base: Booking_schema, db: Session = Depends(get_db)):
+    # Verificar que el usuario y el ticket existen
+    user = db.query(User).filter(User.dni == booking_base.dni).first()
+    booking = db.query(Booking).filter(Booking.id == booking_base.id_booking).first()
+    if not user or not booking:
+        raise HTTPException(status_code=404, detail="User or Ticket not found")
+
+    # Registrar la compra
+    new_purchase = Purchase(
+        dni=booking_base.dni,
+        id_reserva=id_booking,
+        id_ticket=booking_base.id_ticket
+    )
+    db.add(new_purchase)
+    db.commit()
+    db.refresh(new_purchase)
+    return {"message": "Purchase successful", "purchase": new_purchase}
 
 
-@app.post("/suggestions/genre", response_model=Suggestion)
-def get_suggestions_genre(genres: Genres, db: Session = Depends(get_db)):
-    """Endpoint to suggest movies based on user preferences."""
-    try:
-        genres_list = genres.genres
-        # Example: Return top 3 movies for each preferred genre
-        suggestions = []
-        for genre in genres_list:
-            current = crud.get_movies_by_genre(db, genre)[:3]
-            print(current)
-            suggestions.extend(current)
+@app.post("/ticket/cancelation")
+def make_cancelation(booking_base: Booking_schema, db: Session = Depends(get_db)):
+    # Verificar que la reserva y el ticket existen
+    booking = db.query(Booking).filter(Booking.id == booking_base.id_booking).first()
+    user = db.query(User).filter(User.id == booking_base.dni).first()
+    if not booking or not user:
+        raise HTTPException(status_code=404, detail="Booking or Ticket not found")
 
-        logger.info("Exito en ejecucion")
-        print(suggestions)
-        return {"suggestions": suggestions}
-    except Exception as e:
-        logger.error("Error en ejecucion")
-        raise HTTPException(status_code=500, detail="Internal Server Error") from e
+    # Registrar la cancelación y actualizar la reserva
+    new_cancelation = Cancelation(
+        dni=booking_base.dni,
+        id_reserva=id_booking,
+        id_ticket=booking_base.id_ticket
+    )
+    db.add(new_cancelation)
+    ##AQUI VA EL PUT PARA ACTUALIZAR EL ESTADO
+    db.commit()
+    return {"message": "Cancelation successful", "cancelation": new_cancelation}
+
+@app.post("/ticket/booking")
+def make_booking(user_base: User_and_ticket_schema, db: Session = Depends(get_db)):
+    # Verificar que el usuario y el ticket existen
+    user = db.query(User).filter(User.dni == user_base.dni).first()
+    ticket = db.query(Ticket).filter(Ticket.id == user_base.id_ticket).first()
+    if not user or not ticket:
+        raise HTTPException(status_code=404, detail="User or Ticket not found")
+
+    # Registrar la reserva
+    new_booking = Booking(
+        dni = user_base.dni,
+        id_ticket = ticket.id,
+        status = "active"
+    )
+    db.add(new_booking)
+    db.commit()
+    db.refresh(new_booking)
+    return {"message": "Booking successful", "booking": new_booking}
--- app/models.py
+++ app/models.py
@@ -1,32 +1,58 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
 from app.database import Base
 
+class User(Base):
+    __tablename__ = "user"
+    dni = Column(String, primary_key=True, index=True)
+    
+    # Relaciones
+    bookings = relationship("Booking", back_populates="user")
+    purchases = relationship("Purchase", back_populates="user")
+    cancelations = relationship("Cancelation", back_populates="user")
 
 class Ticket(Base):
-    __tablename__ == "ticket"
+    __tablename__ = "ticket"
     id = Column(Integer, primary_key=True, index=True)
-    concierto = Column(string)
-    precio = Column(Integer)
-    
-class Reserva(Base):
-    __tablename_- == "reserva"
+    concert = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+
+    # Relaciones
+    bookings = relationship("Booking", back_populates="ticket")
+    purchases = relationship("Purchase", back_populates="ticket")
+    cancelations = relationship("Cancelation", back_populates="ticket")
+
+class Booking(Base):
+    __tablename__ = "booking"
     id = Column(Integer, primary_key=True, index=True)
-    id_ticket = Column(Integer, primary_key=True, index=True)
-    status = Column(String)
-    
-class Compra(Base):
-    __tablename == "compra"
+    dni = Column(Integer, ForeignKey("user.dni"), nullable=False)
+    id_ticket = Column(Integer, ForeignKey("ticket.id"), nullable=False)
+    status = Column(String, nullable=False)
+
+    # Relaciones
+    user = relationship("User", back_populates="bookings")
+    ticket = relationship("Ticket", back_populates="bookings")
+
+class Purchase(Base):
+    __tablename__ = "purchase"
     id = Column(Integer, primary_key=True, index=True)
-    id_reserva = Column(Integer, primary_key=True, index=True)
-    id_ticket = Column(Integer, primary_key=True, index=True)
-    pay_method = Column(String)
-    
-class Cancelacion(Base):
-    _tablename == "cancelacion"
+    dni = Column(Integer, ForeignKey("user.dni"), nullable=False)
+    id_reserva = Column(Integer, ForeignKey("booking.id"), nullable=False)
+    id_ticket = Column(Integer, ForeignKey("ticket.id"), nullable=False)
+
+    # Relaciones
+    user = relationship("User", back_populates="purchases")
+    ticket = relationship("Ticket", back_populates="purchases")
+    booking = relationship("Booking")
+
+class Cancelation(Base):
+    __tablename__ = "cancelation"
     id = Column(Integer, primary_key=True, index=True)
-    id_reserva = Column(Integer, primary_key=True, index=True)
-    id_ticket = Column(Integer, primary_key=True, index=True)
-    reason = Column(String)
-    
-    
-    
+    dni = Column(Integer, ForeignKey("user.dni"), nullable=False)
+    id_reserva = Column(Integer, ForeignKey("booking.id"), nullable=False)
+    id_ticket = Column(Integer, ForeignKey("ticket.id"), nullable=False)
+
+    # Relaciones
+    user = relationship("User", back_populates="cancelations")
+    ticket = relationship("Ticket", back_populates="cancelations")
+    booking = relationship("Booking")
